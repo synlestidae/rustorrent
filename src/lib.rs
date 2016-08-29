d76898a99f009a1ee5817b148a4762a1ccdc597c
Mate Antunovic <just.mate.antunovic@gmail.com>
2016-08-30 07:35:43 +1200

    Add skeleton for handling requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+extern crate hyper;
+
 pub mod bencode;
 pub mod metainfo;
 pub mod tracker;
 
+mod tracker_http;
 mod convert;
 mod tests;
---
+++ src/tracker_http.rs
@@ -0,0 +1,26 @@
+use tracker::{TrackerReq, TrackerResp};
+use bencode::decode::belement_decode;
+use hyper::Url;
+use hyper::client::Request;
+use hyper::client::Client;
+use hyper::net::HttpStream;
+
+pub trait TrackerHandler {
+    fn request(req: &TrackerReq) -> TrackerResp;
+}
+
+pub struct HttpTrackerHandler {
+    url: Url
+}
+
+impl HttpTrackerHandler {
+    pub fn new(url: Url) -> HttpTrackerHandler {
+        HttpTrackerHandler { url: url  } 
+    }
+}
+
+impl TrackerHandler for HttpTrackerHandler {
+    fn request(_: &TrackerReq) -> TrackerResp {
+        unimplemented!()
+    }
+}
