a45bdcddd806c38e8744ac727e99d4e01656fd60
Maté Antunovic <mate.antunovic@vizexplorer.com>
2016-09-01 11:00:40 +1200

    Make http request work

--- src/tracker_http.rs
+++ src/tracker_http.rs
@@ -1,6 +1,8 @@
 use tracker::{TrackerReq, TrackerResp};
-use bencode::decode::belement_decode;
+use bencode::Bencode;
+use bencode::decode::{belement_decode, DecodeResult};
 use hyper::Url;
+use convert::TryFrom;
 use hyper::client::Request;
 use hyper::client::Client;
 use hyper::net::HttpStream;
@@ -13,25 +15,27 @@ pub trait TrackerHandler {
 }
 
 pub struct HttpTrackerHandler {
-    url: Url
+    url: Url,
 }
 
 impl HttpTrackerHandler {
     pub fn new(url: Url) -> HttpTrackerHandler {
-        HttpTrackerHandler { url: url  } 
+        HttpTrackerHandler { url: url }
     }
 }
 
 #[derive(Debug)]
 pub enum TrackerError {
-    Unknown
+    Unknown,
 }
 
 impl Error for TrackerError {
-    fn description(&self) -> &str { unimplemented!() }
+    fn description(&self) -> &str {
+        unimplemented!()
+    }
 }
 
-impl fmt::Display for TrackerError { 
+impl fmt::Display for TrackerError {
     fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
         unimplemented!()
     }
@@ -39,23 +43,27 @@ impl fmt::Display for TrackerError {
 
 impl TrackerHandler for HttpTrackerHandler {
     fn request(&self, req: &TrackerReq) -> Result<TrackerResp, TrackerError> {
-        //build the url 
+        // build the url
         let mut url = self.url.clone();
-        let query_string = req.to_query_string_pairs().iter()
-            .fold(String::new(), |string, &(ref k, ref v)| format!("{}{}={}", string, k, v));
+        let query_string = req.to_query_string_pairs()
+            .iter()
+            .fold(String::new(),
+                  |string, &(ref k, ref v)| format!("{}{}={}", string, k, v));
         url.set_query(Some(&query_string));
 
-        //make the request
-        let client = Client::with_connector(P::new());
+        // make the request
+        let client = Client::new();
         match client.get(url).send() {
             Ok(mut response) => {
                 let mut response_bytes = Vec::new();
-                response.read_to_end(&mut response_bytes); 
+                response.read_to_end(&mut response_bytes);
                 match belement_decode(&response_bytes).unwrap() {
-                    DecodeResult(Bencode::BDict(dict), _) => Ok(TrackerResp::try_from(dict).unwrap()),
-                    _ => panic!("Incorrect data")
+                    DecodeResult(Bencode::BDict(dict), _) => {
+                        Ok(TrackerResp::try_from(dict).unwrap())
+                    }
+                    _ => panic!("Incorrect data"),
                 }
-            },
+            }
             Err(_) => Err(TrackerError::Unknown),
         }
     }
