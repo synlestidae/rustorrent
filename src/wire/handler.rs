aee7938676c6e86f62f6b894c601861313380404
Mate Antunovic <just.mate.antunovic@gmail.com>
2016-09-19 19:10:12 +1200

    Add handshake method

--- src/wire/handler.rs
+++ src/wire/handler.rs
@@ -3,9 +3,10 @@ use file::PartialFile;
 use metainfo::MetaInfo;
 use convert::TryInto;
 use sha1::{Sha1, Digest};
+use metainfo::SHA1Hash20b;
 
 pub trait PeerHandler {
-    fn handshake(&mut self, info: &MetaInfo, pf: &PartialFile) -> Vec<PeerMsg>;
+    fn handshake(&mut self, info_hash: &SHA1Hash20b, pf: &PartialFile) -> Vec<PeerMsg>;
     fn on_message_receive(&mut self, info: &MetaInfo, pf: &PartialFile, msg: PeerMsg) -> Vec<PeerMsg>;
     fn peer_choking(&mut self, info: &MetaInfo, pf: &PartialFile) -> bool;
     fn peer_interested(&self, info: &MetaInfo, pf: &PartialFile) -> bool;
@@ -22,17 +23,12 @@ impl BasicHandler {
 const _protocol_id: &'static str = "rustorrent-beta";
 
 impl PeerHandler for BasicHandler {
-    fn handshake(&mut self, info: &MetaInfo, pf: &PartialFile) -> Vec<PeerMsg> {
-        /*if let Ok(info) = info.info.try_into() {
-            let sha1 = Sha1::new();
-            let hash = sha1.update(info).digest().bytes();
-            let id = self.id.to_string().into_bytes();
-            id.resize(0);
-            PeerMsg::HandShake(_protocol_id.to_string().into_bytes(), hash, id)
-        } else {
-            vec![]
-        }*/
-        unimplemented!();
+    fn handshake(&mut self, hash: &SHA1Hash20b, pf: &PartialFile) -> Vec<PeerMsg> {
+        /*let sha1 = Sha1::new();
+        let hash = sha1.update(info).digest().bytes();*/
+        let mut id = self.0.to_string().into_bytes();
+        id.resize(20, 0);
+        vec![PeerMsg::HandShake(_protocol_id.to_string(), hash.clone(), id)]
     }
     fn on_message_receive(&mut self, info: &MetaInfo, pf: &PartialFile, msg: PeerMsg) -> Vec<PeerMsg> {
         unimplemented!()
